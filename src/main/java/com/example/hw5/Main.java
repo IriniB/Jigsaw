fcd8e4e31a3a41ce4cf38fc94523b85a69e9ed33
Anastasia <71143459+AnastasiaMorozovaBPI205@users.noreply.github.com>
2023-04-20 22:05:28 +0300

    add fxml

--- src/main/java/com/example/hw5/Main.java
+++ src/main/java/com/example/hw5/Main.java
@@ -1,16 +1,35 @@
 package com.example.hw5;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        new StartingMenu(stage, 0,0);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Tetris");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
