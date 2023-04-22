ba38555824744c2255aacd133fc0f668ff59323c
i.baranyuk <i.baranyuk@fund-f.ru>
2023-04-23 02:04:41 +0300

    liderboard

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.hw5 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.postgresql.jdbc;
 
 
     opens com.example.hw5 to javafx.fxml;
